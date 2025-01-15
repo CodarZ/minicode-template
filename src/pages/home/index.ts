71d54621038c15e2bbef15db2568755cd64d6e0f
codarZ <1476712061@qq.com>
2025-01-15 11:49:38 +0800

    feat: 常用样式和样式目录结构

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,15 +1,8 @@
-import { request } from '@/utils/request'
-
 Page({
-  async onLoad() {
-    try {
-      const { data } = await request.post('/api/sdfsfsdf', {})
-      console.log('请求信息: ', data)
-    } catch (error: any) {
-      wx.showToast({ title: error.message, icon: 'none' })
-    }
+  data: {
+    list: 100,
+  },
+  onLoad() {
     console.log('Home')
   },
-
-  async test() {},
 })
