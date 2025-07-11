05813043e1f424bac0c0cefb727d559717d19a11
CodarZ <1476712061@qq.com>
2025-07-11 10:30:56 +0800

    refactor: Picker Behavior 单列选择器

--- src/behaviors/picker.ts
+++ src/behaviors/picker.ts
@@ -1,29 +1,26 @@
 import { TdPopupProps } from "tdesign-miniprogram/popup/type";
-import dayjs from "dayjs";
 
 /**
  * Tdesign 的 Picker 选择器通用封装
  *
- * @description 提供了通用的选择器行为，包括普通选择器和日期时间选择器
+ * 用于单个选择器使用，多列选择需要再适配
+ *
+ * @description 提供普通 单列 选择器通用封装
  * @module behaviors/picker
  */
 const pickerBehavior = Behavior({
   data: {
     /** 记录当前激活的选择器字段名称 */
     _pickedProp: "",
+    /** 记录当前激活的选择器选项 */
+    _pickedPropDict: [], // 选择器选项数据
+    _pickedPropVisible: false, // 选择器是否可见
+    _pickedPropValue: [], // 选择器的显示值
 
     /** Picker 选择器弹出层属性 */
     _PickerPopupProps: {
       usingCustomNavbar: true,
     } as unknown as TdPopupProps,
-
-    /** DateTimePicker 日期选择器弹出层属性 */
-    _DateTimePickerPopupProps: {
-      usingCustomNavbar: true,
-    } as unknown as TdPopupProps,
-
-    /** 默认显示时间，默认为当前时间 */
-    _DateTimePickerDefault: dayjs().format("YYYY-MM-DD HH:mm:ss"),
   },
   methods: {
     /**
@@ -33,7 +30,22 @@ const pickerBehavior = Behavior({
      */
     _showPicker(e: WechatMiniprogram.CustomEvent) {
       const { prop } = e.currentTarget.dataset;
-      this.setData({ _pickedProp: prop, [`${prop}Visible`]: true });
+      // @ts-ignore
+      const dict = this.data[`${prop}Dict`] || [];
+
+      this.setData(
+        {
+          _pickedPropDict: dict,
+          _pickedProp: prop,
+          // @ts-ignore
+          _pickedPropValue: [this.data[prop]],
+        },
+        () => {
+          this.setData({
+            _pickedPropVisible: true,
+          });
+        }
+      );
     },
 
     /**
@@ -42,26 +54,41 @@ const pickerBehavior = Behavior({
      * @example this._closePicker()
      */
     _closePicker() {
-      const { _pickedProp } = this.data;
-      this.setData({ [`${_pickedProp}Visible`]: false, _pickedProp: "" });
+      this.setData({
+        _pickedProp: "",
+        _pickedPropValue: [],
+        _pickedPropVisible: false,
+      });
     },
 
     /**
-     * 默认的选择器确认事件处理函数
+     * 普通选择器确认事件处理函数
      *
      * @remarks
-     * - 日期选择器 `DateTimePicker` 没有 `label` 属性
      * - 普通选择器 `Picker` 的值为数组类型
      */
-    _onConfirm(e: WechatMiniprogram.CustomEvent) {
+    _onConfirmPicker(e: WechatMiniprogram.CustomEvent) {
       const { value, label } = e.detail;
-      const { prop } = e.target.dataset;
+      const { _pickedProp } = this.data;
 
-      this.setData({
-        [`${prop}Value`]: value,
-        [`${prop}Text`]: label || value,
-        [`${prop}Visible`]: false,
-      });
+      this.setData(
+        {
+          [`${_pickedProp}`]: value[0],
+          [`${_pickedProp}Label`]: label?.[0] || value[0],
+          _pickedPropVisible: false,
+          _pickedPropValue: value,
+        },
+        () => {
+          console.group(`Picker: ${_pickedProp}`);
+          console.log("value:", value);
+          console.log("label:", label);
+          // @ts-ignore
+          console.log(_pickedProp, this.data[_pickedProp]);
+          // @ts-ignore
+          console.log(`${_pickedProp}Label`, this.data[`${_pickedProp}Label`]);
+          console.groupEnd();
+        }
+      );
     },
   },
 });
